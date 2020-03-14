a4a539379811df22b85f65b9057de5559504927c
CHANG-CHING-CHUNG <54635661+CHANG-CHING-CHUNG@users.noreply.github.com>
2020-03-14 14:59:49 +0800

    Added map function

--- app.js
+++ app.js
@@ -36,15 +36,75 @@ $(document).ready(() => {
     }
     
   })
+})
+const mapDiv = document.querySelector('#map');
+let happyHwa = {lat:24.816500, lng:121.026738};
+const map = new google.maps.Map(mapDiv, {
+  center: happyHwa,
+  zoom: 17
+}); 
+const marker = new google.maps.Marker({
+  position: happyHwa,
+  map:map,
+  draggable:true
+});
 
-  const mapDiv = document.querySelector('#map');
-  const initMap = () => {
-    const happyHwa = {lat:24.816500, lng:121.026738};
-    const map = new google.maps.Map(mapDiv, {
-      center: happyHwa,
-      zoom: 18
-    }); 
-    const marker = new google.maps.Marker({position: happyHwa, map:map})
+const circleRange = new google.maps.Circle({
+  strokeColor:'#313131',
+  strokeOpacity:0.3,
+  fillColor:'#fff',
+  fillOpacity:0.4,
+  map:map,
+  center:happyHwa,
+  radius:100
+});
+circleRange.bindTo('center', marker, 'position');
+let bounds = circleRange.getBounds();
+
+const checkAddress = $('#check-address');
+const invalid = $("#invalid");
+let adsLatLng;
+let geocoder = new google.maps.Geocoder();
+let addressMarker;
+
+const getGeocode = (geocoder) => {
+  const address = $('#address').val();
+  geocoder.geocode({'address':address}, (results, status) => {
+
+    if(status ==='OK') {
+      if(addressMarker) {
+      addressMarker.setMap(null);
+      }
+       addressMarker = new google.maps.Marker({
+        map:map,
+        position:results[0].geometry.location
+      });
+
+      adsLatLng = results[0].geometry.location;
+
+      if(bounds.contains(adsLatLng)) {
+        checkAddress.text("你所處的地方在外送範圍內唷~!");
+      } else {
+        checkAddress.text(`很可惜~你所在的位置在服務範圍之外>"<`);
+      }
+    } else {
+      invalid.text("請輸入有效地址!!");
+      setTimeout(()=>{invalid.text('')}, 2000);
+      console.log('Something is wrong!' + status);
+    }
+  });
+}
+
+$('#submit').on('click', function(){
+  getGeocode(geocoder)}
+  );
+  
+google.maps.event.addListener(marker, "dragend", () => {
+  happyHwa = new google.maps.LatLng(marker.position.lat(), marker.position.lng());
+  bounds = circleRange.getBounds();
+  if(bounds.contains(adsLatLng)) {
+    checkAddress.text("你所處的地方在外送範圍內唷~!");
+  } else {
+    checkAddress.text(`很可惜~你所在的位置在服務範圍之外>"<`);
   }
-  initMap();
 })
